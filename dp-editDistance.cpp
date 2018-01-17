ade895b5ca86ffa97adadcc76f749933eedb1376
tanmesh <tanmeshnm@gmail.com>
2018-01-17 21:10:03 +0530

    completed

--- dp-editDistance.cpp
+++ dp-editDistance.cpp
@@ -9,17 +9,21 @@ int dp[100][100];
 int editDistance(int i, int j, string& s1, string& s2) {
 	int &res = dp[i][j];
 	if(res == -1) {
-		if(i == s1.length() || j == s2.length()) {
-			res = 0;
+		if(i == 0) {
+			res = j;
 		}
-		else{
-			if(s1[i] == s2[i]) {
-				res = 1 + editDistance(i+1, j+1, s1, s2);
+		else if(j == 0) {
+			res = i;
+		}
+		else {
+			if(s1[i] == s2[j]) {
+				res = editDistance(i-1, j-1, s1, s2);
 			}
 			else {
-				res = editDistance(i, j+1, s1, s2);
-				res = min(res, editDistance(i+1, j, s1, s2));     // remove
-				res = min(res, editDistance(i+1, j+1, s1, s2));		//  replace
+				int insert  = editDistance(i, j-1, s1, s2);
+				int remove  = editDistance(i-1, j, s1, s2);     
+				int replace = editDistance(i-1, j-1, s1, s2);	
+				res = 1 + min(min(insert, remove), replace);
 			}
 		}
 	}
@@ -28,11 +32,10 @@ int editDistance(int i, int j, string& s1, string& s2) {
 
 int main() {
 	string s1, s2;
-	s1 = "cutee";
-	s2 = "cut";
+	cout << "Enter string\n";
+	cin >> s1 >> s2;
 
 	memset(dp, -1, sizeof(dp));
-	cout << editDistance(0, 0, s1, s2) << endl;
-
+	cout << editDistance(s1.length(), s2.length(), s1, s2) << endl;
 	return 0;
 }
