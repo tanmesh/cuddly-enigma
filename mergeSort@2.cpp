3159b7bc6374ed6039134e099fc2c70be6dfdec1
tanmesh <tanmeshnm@gmail.com>
2018-01-21 21:54:43 +0530

    my mergeSort@2

---
+++ mergeSort@2.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void merge(int* ptr, int l, int h) {
+    int start = l, end = h;
+    int mid = (l+h)/2;
+    int arr[h+1];
+
+    int i = l, j = mid+1, k = 0;
+
+    while(i <= mid && j <= h) {
+        if(*(ptr+i) < *(ptr+j)) {
+            arr[k++] = *(ptr+i);
+            ++i;
+        }
+        else{
+            arr[k++] = *(ptr+j);   
+            ++j;
+        }
+    }
+
+    if(i <= mid) {
+        while(i <= mid) {
+            arr[k++] = *(ptr+i);
+            ++i;
+        }
+    }
+
+    if(j <= h) {
+        while(j <= h) {
+            arr[k++] = *(ptr+j);   
+            ++j;
+        }
+    }
+
+    j = 0;
+    for(int i=start; i<=end; ++i) {
+        *(ptr+i) = arr[j++];
+    } 
+    // ptr = arr;
+}
+
+void print(int arr[], int n) {
+    for(int i=0; i<n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void mergesort(int *ptr, int l, int h) {
+    if(l < h) {
+        int mid = (l+h)/2;
+
+        mergesort(ptr, l, mid);
+        mergesort(ptr, mid+1, h);
+
+        merge(ptr, l, h);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0; i<n; ++i) {
+        cin >> arr[i];
+    }
+
+    mergesort(arr, 0, n-1);
+
+    print(arr, n);
+    return 0;
+}
