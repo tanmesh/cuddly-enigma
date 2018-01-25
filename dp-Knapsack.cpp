f922a53cf6e1e413eb249d02e8eb3db1a9b3bc6d
tanmesh <tanmeshnm@gmail.com>
2018-01-26 01:17:15 +0530

    knap sack

--- dp-Knapsack.cpp
+++ dp-Knapsack.cpp
@@ -3,10 +3,9 @@
 
 using namespace std;
 
-int dp[100];
+int dp[1000];
 
 int knapsack(int totalWeigth, vector<int>& weight, vector<int>& value) {
-
     int& res = dp[totalWeigth];
     if(res == -1) {
         if(totalWeigth == 0) {
@@ -14,8 +13,10 @@ int knapsack(int totalWeigth, vector<int>& weight, vector<int>& value) {
         }
         else{
             for(int i=0; i<weight.size(); ++i) {
-                res = max(value[i] + knapsack(totalWeigth-weight[i], weight, value), 
+                if(totalWeigth-weight[i] >= 0){
+                    res = max(value[i] + knapsack(totalWeigth-weight[i], weight, value), 
                     knapsack(totalWeigth-weight[i], weight, value));
+                }
             }
         }    
     }
@@ -29,8 +30,13 @@ int main() {
     memset(dp, -1, sizeof(dp));
 
     totalWeigth = 100;
-    weight.push_back(1); weight.push_back(50);
+    weight.push_back(1); weight.push_back(50);    
     value.push_back(1); value.push_back(30);
+
+    // totalWeigth = 8;
+    // weight.push_back(1); weight.push_back(3); weight.push_back(4); weight.push_back(5);
+    // value.push_back(10); value.push_back(40); value.push_back(50); value.push_back(70);
+
 
     cout << knapsack(totalWeigth, weight, value) << endl;
 
