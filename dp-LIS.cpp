a90dc6aae230e13109e3595df3a16c10644af9c1
tanmesh <tanmeshnm@gmail.com>
2018-01-28 18:51:43 +0530

    Longest increasing subsequence

---
+++ dp-LIS.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dp[100];
+
+int lis(int i, vector<int> &arr) {
+    int &res = dp[i];
+    if(res == -1) {
+        res = 0;
+        if(i == 0) {
+            res = 1;
+        }
+        else{
+            for(int j=0; j<i; ++j) {
+                if(arr[j] < arr[i]) {
+                    res = max(lis(j, arr)+1, res);
+                }
+            }
+        }
+    }
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    memset(dp, -1, sizeof(dp));
+
+    vector<int> arr;
+    for(int i=0; i<n; ++i) {
+        int tmp;
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+
+    cout << lis(n-1, arr) << endl;
+    
+    return 0;
+}
