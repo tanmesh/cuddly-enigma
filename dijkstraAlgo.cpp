6b28501f139a4989063b8c8057f9ddecad83c2fd
tanmesh <tanmeshnm@gmail.com>
2018-03-28 07:32:29 +0530

    DIJ ALGO

--- dijkstraAlgo.cpp
+++ dijkstraAlgo.cpp
@@ -12,35 +12,28 @@ typedef pair<int,int> PP;
 void dijktra(vector< PP > graph[], int v) {
 	int scr;
 	cin >> scr;
+	--scr;
 	vector<int> dis(v, INT_MAX);
 	dis[scr] = 0;
-	priority_queue < PP > Q;
-	// for(int i=0; i<v; ++i) {
-		Q.push(PP(scr, dis[scr]));
-	// }
+	priority_queue < PP > Q; 
+	Q.push(mp(dis[scr], scr));
+	int cnt = 1;
 	while(!Q.empty()) {
 		int u = Q.top().second;		
 		Q.pop();
-		// for(auto it = graph[u].begin(); it!=graph[u].end(); ++it) {
-		// 	int v = it->f;
-		// 	int wt = it->sc;
-		// 	if(dis[v] > dis[u]+wt) {
-		// 		dis[v] = dis[u]+wt;
-		// 		Q.push(PP(v, -dis[v]));
-		// 	}
-		// }
+		if(cnt == v) break;
 		int sz=graph[u].size();
-	    for(int i=0;i<sz;++i){
-	        int v=graph[u][i].first;
-	        int wt=graph[u][i].second;
-	        if(dis[v]>dis[u]+wt) {
-	            dis[v]=dis[u]+wt;
-	            Q.push(PP(-dis[v],v));
+	    for(int i=0; i<sz; ++i){
+	        int v = graph[u][i].first;
+	        int wt = graph[u][i].second;
+	        if(dis[v] > dis[u]+wt) {
+	            dis[v] = dis[u]+wt;
+	            Q.push(mp(dis[v], v));
 	        }
     	}
 	}
 	for(int i=0;i<v;++i) {
-		cout<<i<<" "<<dis[i]<<endl;
+		cout<<i<<" : "<<dis[i]<<endl;
 	}
 }
 
@@ -55,53 +48,11 @@ int main() {
 	for(int i=0;i<e;i++) {
 		int n, m, w;
 		cin >> n >> m >> w;
+		--n, --m;
 		graph[n].push_back(mp(m, w));
 		graph[m].push_back(mp(n, w));
 	}
 
-	// for(int j=0; j<v; j++) {
-	// 	for(auto it=graph[j].begin(); it!=graph[j].end(); it++) {
-	// 		cout << j << "-" << it->first << " " << it->second << endl;
-	// 	}
-	// }
-
 	dijktra(graph, v);
 	return 0;
 }
-
-// typedef pair<int,int> pp;
-// int main() {
-//     int n,m;
-//     cin>>n>>m;
-//     vector<pp>G[100];
-//     int u,v,w;
-//     for(int i=0;i<m;++i) {
-//         cin>>u>>v>>w;
-//         G[u].pb(mp(v,w));
-//         G[v].pb(mp(u,w));
-//     }
-//     priority_queue<pp>Q;
-//     int d[n+1];
-//     for(int i=1;i<=n;++i)
-//         d[i]=INT_MAX;
-//     int s;
-//     cin>>s;
-//     d[s]=0;
-//     Q.push(pp(0,s));
-//     while(!Q.empty()){
-//         int u=Q.top().second;
-//         cout << u << endl;
-//         Q.pop();
-//         int sz=G[u].size();
-//         for(int i=0;i<sz;++i){
-//             int v=G[u][i].first;
-//             int wt=G[u][i].second;
-//             if(d[v]>d[u]+wt) {
-//                 d[v]=d[u]+wt;
-//                 Q.push(pp(-d[v],v));
-//             }
-//         }
-//     }
-//     // for(int i=1;i<=n;++i)cout<<i<<" "<<d[i]<<endl;
-//      return 0;
-// }
